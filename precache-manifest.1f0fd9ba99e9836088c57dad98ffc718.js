fb5fd8eab041350fd350d600b505a78088326327
jack <1406349834@qq.com>
2021-06-11 17:32:34 +0800

    deploy

---
+++ precache-manifest.1f0fd9ba99e9836088c57dad98ffc718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8195e17fc6aaa6d9684",
+    "url": "/css/app.0d0f7b78.css"
+  },
+  {
+    "revision": "13acfea494faf94b87ba00020f260328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3471de434eac71c78d72",
+    "url": "/js/about.fe95df1d.js"
+  },
+  {
+    "revision": "a8195e17fc6aaa6d9684",
+    "url": "/js/app.3b463fa0.js"
+  },
+  {
+    "revision": "1d4afe13edd4aa9f7e18",
+    "url": "/js/chunk-vendors.9bdc4e16.js"
+  },
+  {
+    "revision": "d1feefc84428856bcfea22466e8a9177",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
